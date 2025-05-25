8c379ec7ef565298406fc3ebcdc15dc70944d8d5
GodzilllaJedi <jedikeithdavis@gmail.com>
2025-05-25 00:17:40 -0500

    Add files via upload

---
+++ Content/SoulToggles/SotsSoulHeader.cs
@@ -0,0 +1,12 @@
+﻿using FargowiltasSouls.Core.Toggler.Content;
+using ssm.SOTS.Souls;
+using Terraria.ModLoader;
+
+namespace ssm.Content.SoulToggles
+{
+    internal class SotsSoulHeader : SoulHeader
+    {
+        public override float Priority => 6.1f;
+        public override int Item => ModContent.ItemType<SotsSoul>();
+    }
+}
