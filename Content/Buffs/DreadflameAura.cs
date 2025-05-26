c557dee430eb933deede68470e054ab09e281702
GodzilllaJedi <jedikeithdavis@gmail.com>
2025-05-26 01:13:41 -0500

    Add files via upload

---
+++ Content/Buffs/DreadflameAura.cs
@@ -0,0 +1,30 @@
+﻿using FargowiltasSouls;
+using ssm.Core;
+using Terraria;
+using Terraria.ModLoader;
+using static ssm.SoA.Enchantments.DreadfireEnchant;
+
+namespace ssm.Content.Buffs
+{
+    [ExtendsFromMod(ModCompatibility.SacredTools.Name)]
+    [JITWhenModsEnabled(ModCompatibility.SacredTools.Name)]
+    public class DreadflameAura : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.buffNoSave[Type] = true;
+            Terraria.ID.BuffID.Sets.NurseCannotRemoveDebuff[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.GetDamage<SummonDamageClass>() += player.ForceEffect<DreadfireEffect>() ? 0.3f : 0.1f;
+            player.GetDamage<MagicDamageClass>() += player.ForceEffect<DreadfireEffect>() ? 0.3f : 0.1f;
+        }
+
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+            npc.lifeRegen -= Main.LocalPlayer.ForceEffect<DreadfireEffect>() ? 30 : 5;
+        }
+    }
+}
